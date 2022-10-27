0ab8fe8906035632c23c81cf52bf259cfdd4e503
YousefAllam221B <yousef.allam221b@gmail.com>
2022-10-27 21:04:26 +0200

    fix getCourse

--- server/controllers/courseController.js
+++ server/controllers/courseController.js
@@ -8,13 +8,13 @@ const getCourse = async (req, res) => {
 		return res.status(404).json({ error: "No such instructor" });
 	}
 
-	const instructor = await Instructor.findById(req.params.id);
+	const course = await Course.findById(req.params.id);
 
-	if (!instructor) {
-		return res.status(404).json({ error: "No such instructor" });
+	if (!course) {
+		return res.status(404).json({ error: "No such Course" });
 	}
 
-	res.status(200).json(instructor);
+	res.status(200).json(course);
 };
 
 // Create a new course
